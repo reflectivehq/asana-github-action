1fd1abd4740b7ad9c3a11124d84f7265a5e2f7d1
Bookis Worthy <bookis.worthy@gmail.com>
2020-07-14 15:17:15 -0700

    Add payload from context

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const taskGid = '1182826486567385';
 
 try {
   console.log(github);
-  const PRUrl = `https://github.com/${github.repository}/pull/${github.payload.number}`;
+  const payload = github.context.payload
+  console.log(payload.repository);
+  const PRUrl = `https://github.com/${github.repository}/pull/${payload.number}`;
   const accessToken = core.getInput('access-token') || process.env.ASANA_ACCESS_TOKEN;
   const client = Asana.Client.create().useAccessToken(accessToken);
   const data = {
-    html_text: `<body>Github PR: <a href="${PRUrl}">#${github.event.number}</a></body>`,
+    html_text: `<body>Github PR: <a href="${PRUrl}">#${payload.number}</a></body>`,
     is_pinned: true,
   };
 
