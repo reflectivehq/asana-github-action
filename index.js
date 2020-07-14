7c756a0b1dcac94caef7ccace58a44deb92f6acb
Bookis Worthy <bookis.worthy@gmail.com>
2020-07-14 15:31:37 -0700

    Debug body of PR

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ const Asana = require('asana');
 
 const taskGid = '1182826486567385';
 
+
 try {
-  const payload = github.context.payload
-  console.log(payload.pull_request);
-  const PRUrl = `${payload.repository.html_url}/${payload.number}`;
+  const { number, pull_request: { _links, body } } = github.context.payload
+
+  console.log(body);
+  const PRUrl = _links.html.href;
   const accessToken = core.getInput('access-token') || process.env.ASANA_ACCESS_TOKEN;
   const client = Asana.Client.create().useAccessToken(accessToken);
   const data = {
-    html_text: `<body>Github PR: <a href="${PRUrl}">#${payload.number}</a></body>`,
+    html_text: `<body>Github PR: <a href="${PRUrl}">#${number}</a></body>`,
     is_pinned: true,
   };
 
