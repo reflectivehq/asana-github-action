70a60a4abee72b024f0709d550ecd65933f577d6
Bookis Worthy <bookis.worthy@gmail.com>
2020-07-17 11:38:03 -0700

    Change deep link to redirect

--- index.js
+++ index.js
@@ -16,16 +16,13 @@ try {
   const accessToken = core.getInput('access-token') || process.env.ASANA_ACCESS_TOKEN;
   const client = Asana.Client.create().useAccessToken(accessToken);
   const herokuUrl = `https://reflective-pr-${number}.herokuapp.com`;
-  const reflectiveDeepUrl = `reflective://switch-host?host=${herokuUrl}`;
+  const reflectiveDeepUrl = `https://link.reflective.co/switch-host?host=${herokuUrl}`;
   const data = {
     html_text: `<body>Github PR: <a href="${reflectiveDeepUrl}">Open Build for PR #${number} in the Reflective App</a></body>`,
     is_pinned: true,
   };
 
-  console.log(data);
-
   client.stories.createStoryForTask(taskGid, data).catch((err) => {
-    console.log(err.value.errors[0]);
     core.setFailed(err.value.errors[0]);
   });
 } catch (error) {
